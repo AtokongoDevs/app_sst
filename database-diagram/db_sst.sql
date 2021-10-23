c6896c991a40ba3e34cafb581bfee8b4e541d845
MiguelAML <92906098+MiguelAML@users.noreply.github.com>
2021-10-22 22:25:51 -0500

    update db_sst

--- database-diagram/db_sst.sql
+++ database-diagram/db_sst.sql
@@ -2,24 +2,24 @@ CREATE DATABASE db_sst;
 USE DATABASE db_sst;
 CREATE TABLE usuario(
 	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(100) NULL,
-	contraseña VARCHAR(100) NULL,
-	rol VARCHAR(100) NULL,
-	estado ENUM('Activo', 'Inactivo','Desactivado') NULL,
-	fecha_creacion DATE NULL,
+	email VARCHAR(100) NOT NULL,
+	contraseña VARCHAR(100) NOT NULL,
+	rol VARCHAR(100) NOT NULL,
+	estado ENUM('Activo', 'Inactivo','Desactivado') NOT NULL,
+	fecha_creacion DATE NOT NULL,
 	fecha_actualizacion DATETIME NULL,
 	PRIMARY KEY (id)
 );
   
 CREATE TABLE detalle_usuario(
 	id INT NOT NULL AUTO_INCREMENT,
-	id_usuario VARCHAR(100) NULL,
-	nombre VARCHAR(100) NULL,
-	apellido VARCHAR(100) NULL,
-	cargo VARCHAR(100) NULL,
-	fecha_creacion DATE NULL,
+	id_usuario VARCHAR(100) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	cargo VARCHAR(100) NOT NULL,
+	fecha_creacion DATE NOT NULL,
 	fecha_actualizacion DATETIME NULL,
-	id_empresa VARCHAR(100) NULL,
+	id_empresa VARCHAR(100) NOT NULL,
 	PRIMARY KEY (id)
 	FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 	FOREIGN KEY (id_empresa) REFERENCES empresa(id)
@@ -27,10 +27,10 @@ CREATE TABLE detalle_usuario(
 	
 CREATE TABLE empresa(
 	id INT NOT NULL AUTO_INCREMENT,
-	razon_social VARCHAR(45) NULL,
-	ruc VARCHAR(45) NULL,
-	direccion_fiscal VARCHAR(100) NULL,
-	fecha_creacion DATE NULL,
+	razon_social VARCHAR(45) NOT NULL,
+	ruc VARCHAR(45) NOT NULL,
+	direccion_fiscal VARCHAR(100) NOT NULL,
+	fecha_creacion DATE NOT NULL,
 	nombre_comercial VARCHAR(100) NULL,
 	PRIMARY KEY (id)
  );
