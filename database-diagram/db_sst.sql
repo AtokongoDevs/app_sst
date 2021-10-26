680cd6e297e2ff6df716e318182556ed101269b0
MiguelAML <92906098+MiguelAML@users.noreply.github.com>
2021-10-26 10:51:30 -0500

    Agregado de character set y motor.
    Script usado para la creación de la base de datos SST

--- database-diagram/db_sst.sql
+++ database-diagram/db_sst.sql
@@ -1,36 +1,39 @@
-CREATE DATABASE db_sst;
-USE DATABASE db_sst;
-CREATE TABLE usuario(
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(100) NOT NULL,
-	contraseña VARCHAR(100) NOT NULL,
-	rol VARCHAR(100) NOT NULL,
-	estado ENUM('Activo', 'Inactivo','Desactivado') NOT NULL,
-	fecha_creacion DATE NOT NULL,
-	fecha_actualizacion DATETIME NULL,
+USE u349685578_sst;
+
+CREATE TABLE usuario (
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+	email VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	contraseña VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	rol VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	estado ENUM('Activo','Inactivo','Desactivado') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'Inactivo' , 
+	fecha_creacion DATE NOT NULL , 
+	fecha_actualizacion DATETIME NULL , 
 	PRIMARY KEY (id)
-);
-  
-CREATE TABLE detalle_usuario(
-	id INT NOT NULL AUTO_INCREMENT,
-	id_usuario VARCHAR(100) NOT NULL,
-	nombre VARCHAR(100) NULL,
-	apellido VARCHAR(100) NULL,
-	cargo VARCHAR(100) NULL,
-	fecha_creacion DATE NOT NULL,
-	fecha_actualizacion DATETIME NULL,
-	id_empresa VARCHAR(100) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-	FOREIGN KEY (id_empresa) REFERENCES empresa(id)
-);
-	
-CREATE TABLE empresa(
-	id INT NOT NULL AUTO_INCREMENT,
-	razon_social VARCHAR(45) NULL,
-	ruc VARCHAR(45) NULL,
-	direccion_fiscal VARCHAR(100) NULL,
-	fecha_creacion DATE NOT NULL,
-	nombre_comercial VARCHAR(100) NULL,
+) ENGINE = InnoDB;
+
+CREATE TABLE empresa (
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+	razon_social VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	ruc VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	direccion_fiscal VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	fecha_creacion DATE NOT NULL , 
+	nombre_comercial VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
 	PRIMARY KEY (id)
- );
+) ENGINE = InnoDB;
+
+CREATE TABLE detalle_usuario ( 
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+	id_usuario INT(11) UNSIGNED NOT NULL , 
+	nombre VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	apellido VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	cargo VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	fecha_creacion DATE NOT NULL , 
+	fecha_actualizacion DATETIME NOT NULL , 
+	id_empresa INT(11) UNSIGNED NOT NULL , 
+	PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+ALTER TABLE detalle_usuario
+	ADD FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE detalle_usuario 
+	ADD FOREIGN KEY (id_empresa) REFERENCES empresa(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
